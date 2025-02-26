15aae778a3aa52726332b2e9a922f4449ab502f3
unknown <elmurodvkh@gmail.com>
2025-02-26 10:14:12 +0500

    create express server

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("Hello world!");
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Welcome to the Subscription Tracker API!");
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+    console.log(`Subscription Tracker API is running on http://localhost:${PORT}`);
+});
+
+export default app;
