7a5f3b60c96f0547df3a30a50fa45f14697f1ec7
unknown <elmurodvkh@gmail.com>
2025-02-27 20:25:26 +0500

    config

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 import express from "express";
+import { PORT } from "./config/env.js";
+
 
 const app = express();
 
 app.get("/", (req, res) => {
     res.send("Welcome to the Subscription Tracker API!");
 });
-
-const PORT = 5000;
 
 app.listen(PORT, () => {
     console.log(`Subscription Tracker API is running on http://localhost:${PORT}`);
