be0e2facb340bba493153b37113b38b50fd9b892
unknown <elmurodvkh@gmail.com>
2025-03-03 19:41:39 +0500

    error handler

--- app.js
+++ app.js
@@ -5,12 +5,20 @@ import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import subRouter from "./routes/sub.routes.js";
 import conntectToDatabase from "./database/mongodb.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subs", subRouter);
+
+app.use(errorMiddleware);
 
 app.get("/", (req, res) => {
     res.send("Welcome to the Subscription Tracker API!");
